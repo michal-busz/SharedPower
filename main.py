7facc36d1b115cd51221e3511a6727366bf88188
Lancher <michalb@odecca.pl>
2018-11-15 15:05:28 +0000

    sample code added to analyze

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from activities.menu import menu
-from tkinter import Tk
+#from activities.menu import menu
+#from tkinter import Tk
 
-root = Tk()
-app = menu(root)
-root.mainloop()
+#root = Tk()
+#app = menu(root)
+#root.mainloop()
+#OLD CODE
+from test.app import SampleApp
+
+app = SampleApp()
+app.mainloop()
